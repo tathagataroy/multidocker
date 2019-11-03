54af46eed6c91ec203020536ee2e92a0a33fd4f6
Tathagata Roy <troy@Tathagatas-MacBook-Air-2.local>
2019-11-03 11:12:24 +0530

    remaining commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
+import OtherPage from './Otherpage';
+import Fibs from './Fibs';
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Link to="/">Home</Link>
+        <Link to="/otherpage">Other Page</Link>
+      </header>
+      <div>
+        <Route exact path="/" component= {Fibs}></Route>
+        <Route path="/otherpage" component={OtherPage}>
+
+        </Route>
+      </div>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Otherpage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+export default () => {
+    return <div>
+        I am some other Page
+        <Link to="/">Go Back Home</Link>
+    </div>
+}
